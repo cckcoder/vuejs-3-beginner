7d60811a050bc44e80f90c50be4396156828909f
chaiwat.chaobaankoh <chaiwat.chaobaankoh@accenture.com>
2023-04-05 14:51:34 +0700

    update: clean up unuse css
    Signed-off-by: chaiwat.chaobaankoh <chaiwat.chaobaankoh@accenture.com>

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>Welcome Home</h1>
   </main>
 </template>
+
+<style>
+</style>
