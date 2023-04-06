c28f61a08666fb50318adb765c87120d992c1e83
codewizz_thinkpad <c.chaiwat.ch@gmail.com>
2023-04-06 15:03:01 +0700

    update: setup context
    Signed-off-by: codewizz_thinkpad <c.chaiwat.ch@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,7 @@ import { RouterLink, RouterView } from 'vue-router'
     <div class="wrapper">
       <nav>
         <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/todo">Todo</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
---
+++ src/views/TodoView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>My Todo List</h1>
+    <ul>
+      <li>
+        Todo 1 -- <span>Description</span>
+      </li>
+      <li>
+        Todo 2 -- <span>Description</span>
+      </li>
+      <li>
+        Todo 3 -- <span>Description</span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const todos = ref([
+  { id: 1, title: 'Drink Coffee', descrip: 'Awesome' },
+  { id: 2, title: 'Learn Vue3', descrip: 'Vue js very cool' },
+  { id: 3, title: 'Play with cat', descrip: 'Cat is my lovely pet!' },
+
+])
+</script>
+
