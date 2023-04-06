1f5d08ddc07a5f3bf78567a9139f85c0f16b8c54
codewizz_thinkpad <c.chaiwat.ch@gmail.com>
2023-04-06 15:18:33 +0700

    update: prepare to v-if lesson
    Signed-off-by: codewizz_thinkpad <c.chaiwat.ch@gmail.com>

--- src/views/TodoView.vue
+++ src/views/TodoView.vue
@@ -2,7 +2,11 @@
   <div>
     <h1>My Todo List</h1>
     <ul>
-      <li v-for="todo in todos" :key="todo.id">
+      <li 
+        v-for="todo in todos" 
+        :key="todo.id"
+        @click="handleToggle(todo.id)"
+      >
         {{ todo.title }} -- 
         <span>{{ todo.descript }}</span>
       </li>
@@ -17,7 +21,21 @@ const todos = ref([
   { id: 1, title: 'Drink Coffee', descript: 'Awesome', isComplete: false },
   { id: 2, title: 'Learn Vue3', descript: 'Vue js very cool', isComplete: false },
   { id: 3, title: 'Play with cat', descript: 'Cat is my lovely pet!', isComplete: false },
-
 ])
+
+const handleToggle = (todoId) => {
+  console.log(todoId)
+}
 </script>
+
+<style scoped>
+ul {
+  cursor: pointer;
+}
+
+li {
+  padding: 5px;
+}
+
+</style>
 
